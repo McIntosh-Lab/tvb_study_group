eecebb90997840267f1da0ba970a1538a94dcd55
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2024-04-26 18:31:27 -0600

    Update PSE_job.sh

--- Session_5-Identifying_and_evaluating_optimal_simulations/2_PSE_all_metrics/PSE_job.sh
+++ Session_5-Identifying_and_evaluating_optimal_simulations/2_PSE_all_metrics/PSE_job.sh
@@ -11,7 +11,7 @@
 ### ====================================
 
 # Location of your submission script directory
-SUBMISSION_SCRIPT_DIR='/path/to/Session_5-Jobs_and_PSE'
+SUBMISSION_SCRIPT_DIR='/path/to/Session_5-Jobs_and_PSE/2_PSE_all_metrics'
 
 # Load in env
 module load scipy-stack
